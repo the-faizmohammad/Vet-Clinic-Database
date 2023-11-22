e99a2d48fa16db87f1f3aecd1534017655f2a160
user-faiz <email2faizm@gmail.com>
2023-11-23 00:43:39 +0530

    add queries to provide answer

--- queries.sql
+++ queries.sql
@@ -1,3 +1,23 @@
-/*Queries that provide answers to the questions from all projects.*/
+-- Query 1
+SELECT * FROM animals WHERE name LIKE '%mon';
 
-SELECT * from animals WHERE name = 'Luna';
+-- Query 2
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+
+-- Query 3
+SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
+
+-- Query 4
+SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+
+-- Query 5
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+
+-- Query 6
+SELECT * FROM animals WHERE neutered = true;
+
+-- Query 7
+SELECT * FROM animals WHERE name <> 'Gabumon';
+
+-- Query 8
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
