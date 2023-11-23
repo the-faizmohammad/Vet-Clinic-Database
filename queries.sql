53a0531847a8c448ee3e8df4d801f73d024a1fb6
user-faiz <email2faizm@gmail.com>
2023-11-23 12:37:28 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -21,3 +21,35 @@ SELECT * FROM animals WHERE name <> 'Gabumon';
 
 -- Query 8
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- Transaction 1: Update species to 'unspecified' and roll back
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals; -- Verify the change
+ROLLBACK;
+SELECT * FROM animals; -- Verify rollback
+
+-- Transaction 2: Update species based on conditions and commit
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals; -- Verify changes
+COMMIT;
+SELECT * FROM animals; -- Verify changes persist after commit
+
+-- Transaction 3: Delete all records and roll back
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals; -- Verify deletion
+ROLLBACK;
+SELECT * FROM animals; -- Verify rollback
+
+-- Transaction 4: Delete animals born after Jan 1, 2022, and update weights
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT weight_update;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO weight_update;
+UPDATE animals SET weight_kg = ABS(weight_kg) WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals; -- Verify changes
