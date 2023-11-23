996fb68ec6482f4e33a0dc2157ff849d66d269ec
user-faiz <email2faizm@gmail.com>
2023-11-23 18:53:41 +0530

    Update Schema.sql

--- schema.sql
+++ schema.sql
@@ -33,3 +33,28 @@ ALTER TABLE animals
     DROP COLUMN species,
     ADD FOREIGN KEY (species_id) REFERENCES species(id),
     ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+-- Join Tables Schema
+
+-- Create the vets table
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    age INTEGER,
+    date_of_graduation DATE
+);
+
+-- Create the specializations table (join table for species and vets)
+CREATE TABLE specializations (
+    vet_id INTEGER REFERENCES vets(id),
+    species_id INTEGER REFERENCES species(id),
+    PRIMARY KEY (vet_id, species_id)
+);
+
+-- Create the visits table (join table for animals and vets)
+CREATE TABLE visits (
+    animal_id INTEGER REFERENCES animals(id),
+    vet_id INTEGER REFERENCES vets(id),
+    visit_date DATE,
+    PRIMARY KEY (animal_id, vet_id, visit_date)
+);
